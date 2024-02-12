c9943fdc471e4b2e461e57062691ce7fa05b3722
NanguRepo <nanguthenangu@gmail.com>
2024-02-12 15:42:08 +0100

    flesh out info command

--- app/src/lib/commands/info.ts
+++ app/src/lib/commands/info.ts
@@ -1,19 +1,37 @@
 import axios from 'axios';
 import { fileSystem } from '$lib/stores';
 import { get } from 'svelte/store';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
-export default async (input: string[]) => {
+const startTime = dayjs();
+dayjs.extend(relativeTime);
+const headerStyle = 'font-weight: 600';
+
+export default async () => {
 	const version = await axios.get('https://api.github.com/repos/nangurepo/terminal/commits/master');
 	return [
-		{ text: 'Terminal\n', style: 'font-size: 24px' },
-		{ text: 'Version: ', style: 'font-weight: 600' },
+		{ text: 'Terminal, a ', style: 'font-size: 24px' },
+		{
+			text: 'pege.io',
+			style: 'font-size: 24px',
+			url: 'https://pege.io'
+		},
+		{ text: ' project\n', style: 'font-size: 24px' },
+		{ text: 'Version: ', style: headerStyle },
 		{
 			text: version.data.sha.substring(0, 7) + '\n',
 			url: 'https://github.com/NanguRepo/terminal/commit/' + version.data.sha
 		},
+		{ text: 'Last updated: ', style: headerStyle },
+		{ text: dayjs(version.data.commit.author.date).from(dayjs()) + '\n' },
 		{
-			text: 'Filesystem size: ' + (JSON.stringify(get(fileSystem)).length / 1000).toFixed(2) + ' kB'
-		}
+			text: 'Filesystem size: ',
+			style: headerStyle
+		},
+		{ text: (JSON.stringify(get(fileSystem)).length / 1000).toFixed(2) + ' kB\n' },
+		{ text: 'Uptime: ', style: headerStyle },
+		{ text: startTime.from(dayjs(), true) }
 	];
 };
 
