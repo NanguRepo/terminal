323a731b9215e9dc4492667b58faf0190e562bbb
NanguRepo <nanguthenangu@gmail.com>
2024-02-17 16:58:10 +0100

    fix a small syntactical imperfection in echo module

--- app/src/lib/commands/echo.ts
+++ app/src/lib/commands/echo.ts
@@ -4,9 +4,6 @@ export default (input: string[]) => {
 	if (!input[0]) {
 		return errorMessage('no input', 'string required');
 	}
-	if (!input[1]) {
-		input[1] = '';
-	}
-	return [{ text: input[0], style: input[1] }];
+	return [{ text: input[0], style: input[1] ? input[1] : '' }];
 };
 export const description = 'print a string to the terminal.';
